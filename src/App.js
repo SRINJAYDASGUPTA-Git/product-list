b4a7b189afd0303d053bb57be1812fa0106ed176
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-11-01 09:52:25 +0530

    TEST

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <h1 className="text-3xl font-bold underline">Hello world! Product List</h1>
   );
 }
 
